5c26161f0d99012e6c538af9021a1f4206b02830
Adrian Lang <lang@cosmocode.de>
2009-11-24 16:44:04 +0100

    German translation.

---
+++ lang/de/lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['search_section_title'] = 'Getaggte Seiten';
+$lang['search_nr_users']      = '(von %d Nutzern)';
---
+++ lang/de/settings.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['db_dsn'] = 'Der Data Source Name für die zu verwendende Datenbank (mysql://benutzername:passwt@host/datenbank)';
+$lang['db_prefix'] = 'Ein Präfix für die Datenbank-Tabellennamen';

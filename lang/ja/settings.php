d8af75bb63a6ea233c0226fb8792c9bfe74d1232
Hideaki SAWADA <chuno@live.jp>
2015-06-21 13:58:19 +0900

    Create settings.php

---
+++ lang/ja/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['cloudlimit']     = 'ページ内のタグクラウドで表示する最大タグ数。0 設定の場合、全タグを表示する。';
+$lang['singleusermode'] = '単一ユーザーモード。全てのタグは誰でも編集可能です。';
+$lang['hiddenprefix']   = '書き込み権限のないユーザーには、この接頭辞のタグを表示しません。';

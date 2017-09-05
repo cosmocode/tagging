4894885b99f0fd06c3b0421d77c6063324a051c8
Szymon Olewniczak <solewniczak@imz.re>
2017-09-05 09:42:09 +0200

    fix lang typo

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -17,7 +17,7 @@ $lang['admin tag does not exists'] = 'The tag name you entered does not exist.';
 $lang['admin renamed']             = 'Your tag has been renamed';
 $lang['admin delete_selected']     = 'Delete Selected';
 $lang['admin filter button']       = 'Filter';
-$lang['admin deleted']             = 'Your have deleted %d tags on %d pages';
+$lang['admin deleted']             = 'You have deleted %d tags on %d pages';
 
 $lang['search_section_title'] = 'Pages tagged';
 $lang['js']['notags']         = 'No tags, yet';

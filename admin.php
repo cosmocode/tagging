57e4530423023c2291dfc4b90cd4c34dbd0f5a35
Szymon Olewniczak <solewniczak@imz.re>
2017-09-06 10:44:35 +0200

    use direct select in getAllTags

--- admin.php
+++ admin.php
@@ -170,9 +170,12 @@ class admin_plugin_tagging extends DokuWiki_Admin_Plugin {
         $form->addTagClose('tr');
 
         foreach ($tags as $taginfo) {
+            sort($taginfo['taggers']);
+            sort($taginfo['orig']);
+            
             $tagname = $taginfo['tid'];
-            $taggers = $taginfo['taggers'];
-            $written = $taginfo['orig'];
+            $taggers = implode(', ', $taginfo['taggers']);
+            $written = implode(', ', $taginfo['orig']);
 
             $form->addTagOpen('tr');
             $form->addTagOpen('td')->addClass('centeralign');

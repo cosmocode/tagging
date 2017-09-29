6bb5774fd94f1577a5f6949b1d96175b40847352
Michael Große <grosse@cosmocode.de>
2017-09-29 16:39:52 +0200

    feat: update language files
    Deleted two unused string from German and Korean and updated the German
    translations.

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -3,17 +3,30 @@
 // if set here, the plugin doesn't need to override the getMenuText() method
 $lang['menu'] = 'Tagging Einstellungen';
 
-$lang['admin headline']            = 'Tagging Plugin';
 $lang['admin tag']                 = 'Tagname';
 $lang['admin occurrence']          = 'Häufigkeit';
+$lang['admin writtenas']           = 'Geschrieben als';
+$lang['admin taggers']             = 'Genutzt von';
 $lang['admin save']                = 'Speichern';
 $lang['admin find tag']            = 'Finde Tag';
 $lang['admin new name']            = 'Neuer Name';
 $lang['admin rename tag']          = 'Tag umbenennen';
+$lang['admin filter']              = 'Nach Namensraum filtern';
 $lang['admin enter tag names']     = 'Bitte geben Sie den aktuellen und den neuen Tagnamen ein';
 $lang['admin tag does not exists'] = 'Ihr eingegebener Tagname existiert nicht';
-$lang['admin saved']               = 'Gespeichert';
+$lang['admin renamed']             = 'Der Tag wurde umbenannt';
+$lang['admin delete_selected']     = 'Ausgewählte Tags löschen';
+$lang['admin filter button']       = 'Filter';
+$lang['admin deleted']             = 'Sie haben %d Tags auf %d Seiten gelöscht';
+$lang['admin sort ascending']      = 'Aufsteigend sortieren';
+$lang['admin sort descending']     = 'Absteigend sortieren';
+$lang['toggle admin mode']         = 'Alle Tags bearbeiten';
+
+$lang['no_admin']                  = 'Fehler. Nur Admins können Tags bearbeiten.';
 
 $lang['search_section_title'] = 'Seiten mit Tag';
 $lang['js']['notags']         = 'Keine Tags vergeben';
 $lang['js']['nopages']        = 'Keine Seiten mit diesem Tag';
+$lang['js']['admin_change_tag']    = 'Tag bearbeiten';
+
+$lang['tagjmp_error'] = 'Es konnte keine Seite mit Tag %s gefunden werden';
---
+++ lang/de/settings.php
@@ -0,0 +1,4 @@
+<?php
+$lang['cloudlimit']     = 'Maximale Anzahl an Tags die in einer Tag-Cloud angezeigt werden. 0 zeigt alle Tags an.';
+$lang['singleusermode'] = 'Single-User-Mode aktivieren. D.h. alle Tags können von allen bearbeitet werden.';
+$lang['hiddenprefix']   = 'Tags mit diesem Prefix werden Nutzern, die nur die Leseberechtigung für eine Seite haben, nicht angezeigt.';

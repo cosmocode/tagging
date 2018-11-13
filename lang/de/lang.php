3bbb8b9b617360c97791d7a1586a472334bf2d2a
Andreas Gohr <gohr@cosmocode.de>
2018-11-13 14:45:37 +0100

    better german translation. fixes #21

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,32 +1,32 @@
 <?php
 // for admin plugins, the menu prompt to be displayed in the admin menu
 // if set here, the plugin doesn't need to override the getMenuText() method
-$lang['menu'] = 'Tagging Einstellungen';
+$lang['menu'] = 'Schlagwort Einstellungen';
 
 $lang['admin tag']                 = 'Tagname';
 $lang['admin occurrence']          = 'Häufigkeit';
 $lang['admin writtenas']           = 'Geschrieben als';
 $lang['admin taggers']             = 'Genutzt von';
 $lang['admin save']                = 'Speichern';
-$lang['admin find tag']            = 'Finde Tag';
+$lang['admin find tag']            = 'Finde Schlagwort';
 $lang['admin new name']            = 'Neuer Name';
-$lang['admin rename tag']          = 'Tag umbenennen';
+$lang['admin rename tag']          = 'Schlagwort umbenennen';
 $lang['admin filter']              = 'Nach Namensraum filtern';
-$lang['admin enter tag names']     = 'Bitte geben Sie den aktuellen und den neuen Tagnamen ein';
-$lang['admin tag does not exists'] = 'Ihr eingegebener Tagname existiert nicht';
-$lang['admin renamed']             = 'Der Tag wurde umbenannt';
-$lang['admin delete_selected']     = 'Ausgewählte Tags löschen';
+$lang['admin enter tag names']     = 'Bitte geben Sie das aktuelle und das neue Schlagwort ein';
+$lang['admin tag does not exists'] = 'Ihr eingegebenes Schlagwort existiert nicht';
+$lang['admin renamed']             = 'Das Schlagwort wurde umbenannt';
+$lang['admin delete_selected']     = 'Ausgewählte Schlagworte löschen';
 $lang['admin filter button']       = 'Filter';
-$lang['admin deleted']             = 'Sie haben %d Tags auf %d Seiten gelöscht';
+$lang['admin deleted']             = 'Sie haben %d Schlagworte auf %d Seiten gelöscht';
 $lang['admin sort ascending']      = 'Aufsteigend sortieren';
 $lang['admin sort descending']     = 'Absteigend sortieren';
 $lang['toggle admin mode']         = 'Alle Tags bearbeiten';
 
-$lang['no_admin']                  = 'Fehler. Nur Admins können Tags bearbeiten.';
+$lang['no_admin']                  = 'Fehler. Nur Admins können Schlagworte bearbeiten.';
 
-$lang['search_section_title'] = 'Seiten mit Tag';
-$lang['js']['notags']         = 'Keine Tags vergeben';
-$lang['js']['nopages']        = 'Keine Seiten mit diesem Tag';
-$lang['js']['admin_change_tag']    = 'Tag bearbeiten';
+$lang['search_section_title'] = 'Seiten mit Schlagwort';
+$lang['js']['notags']         = 'Keine Sclagworte vergeben';
+$lang['js']['nopages']        = 'Keine Seiten mit diesem Schlagwort';
+$lang['js']['admin_change_tag']    = 'Schlagworte bearbeiten';
 
-$lang['tagjmp_error'] = 'Es konnte keine Seite mit Tag %s gefunden werden';
+$lang['tagjmp_error'] = 'Es konnte keine Seite mit Schlagwort %s gefunden werden';
--- lang/de/settings.php
+++ lang/de/settings.php
@@ -1,4 +1,4 @@
 <?php
-$lang['cloudlimit']     = 'Maximale Anzahl an Tags die in einer Tag-Cloud angezeigt werden. 0 zeigt alle Tags an.';
-$lang['singleusermode'] = 'Single-User-Mode aktivieren. D.h. alle Tags können von allen bearbeitet werden.';
-$lang['hiddenprefix']   = 'Tags mit diesem Prefix werden Nutzern, die nur die Leseberechtigung für eine Seite haben, nicht angezeigt.';
+$lang['cloudlimit']     = 'Maximale Anzahl an Schlagworten die in einer Schlagwort-Wolke angezeigt werden. 0 zeigt alle Schlagworte an.';
+$lang['singleusermode'] = 'Single-User-Mode aktivieren. D.h. alle Schlagworte können von allen bearbeitet werden.';
+$lang['hiddenprefix']   = 'Schlagworte mit diesem Prefix werden Nutzern, die nur die Leseberechtigung für eine Seite haben, nicht angezeigt.';

987c50ad55f66cf4d432d1494ae26dc57eabced7
Christian Sandmeier <sandmeier@.vivai.de>
2019-04-22 13:51:03 +0200

    pid link logs

--- helper.php
+++ helper.php
@@ -247,7 +247,7 @@ class helper_plugin_tagging extends DokuWiki_Plugin {
             $pid = '"' . $pid . '"';
         }
 
-        $ret = "test"; //rawurlencode($pid);
+        $ret = $ns; //rawurlencode($pid);
         /*if ($ns) {
             $ret .= "rawurlencode(' @' . $ns)";
         }*/

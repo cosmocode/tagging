26c4179dc6456019ba915da2c1e15436acbf3f61
Anna Dabrowska <dabrowska@cosmocode.de>
2019-10-07 12:01:08 +0200

    Add config to disable namespace column in overview tables

--- conf/metadata.php
+++ conf/metadata.php
@@ -2,3 +2,4 @@
 $meta['cloudlimit']     = array('numeric', '_min' => 0);
 $meta['singleusermode'] = array('onoff');
 $meta['hiddenprefix']   = array('string');
+$meta['hidens'] = array('onoff');
--- lang/de/settings.php
+++ lang/de/settings.php
@@ -7,3 +7,4 @@
 $lang['cloudlimit']            = 'Maximale Anzahl an Schlagworten die in einer Schlagwort-Wolke angezeigt werden. 0 zeigt alle Schlagworte an.';
 $lang['singleusermode']        = 'Single-User-Mode aktivieren. D.h. alle Schlagworte können von allen bearbeitet werden.';
 $lang['hiddenprefix']          = 'Schlagworte mit diesem Prefix werden Nutzern, die nur die Leseberechtigung für eine Seite haben, nicht angezeigt.';
+$lang['hidens']                = 'Namensraum-Spalte in den Übersichtstabellen ausblenden.';

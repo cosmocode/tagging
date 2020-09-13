6eb41c5b337c784c972094ac567ff028d92ce126
Aleksandr Selivanov <alexgearbox@yandex.ru>
2020-09-13 21:40:06 +0200

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['menu']                  = 'Просмотр и изменение тегов';
+$lang['admin tag']             = 'Тег';
+$lang['admin tags']            = 'Теги';
+$lang['admin nonexistent page'] = 'Несуществующая страница с тегами';
+$lang['admin occurrence']      = 'Счётчик';
+$lang['admin namespaces']      = 'Пространства имён';
+$lang['admin taggers']         = 'Сохранено пользователем';
+$lang['admin actions']         = 'Действия';
+$lang['admin save']            = 'Сохранить';
+$lang['admin find tag']        = 'Найти тег';
+$lang['admin new name']        = 'Новое название';
+$lang['admin rename tag']      = 'Переименовать тег';
+$lang['admin rename']          = 'Переименовать';
+$lang['admin filter']          = 'Фильтр по пространству имён';
+$lang['admin renamed']         = 'Ваш тег переименован';
+$lang['admin delete_selected'] = 'Удалить веделенное';
+$lang['admin delete']          = 'Удалить';
+$lang['admin filter button']   = 'Фильтр';
+$lang['admin sort ascending']  = 'Сортировать по возрастанию';
+$lang['admin sort descending'] = 'Сортировать по убыванию';
+$lang['admin no invalid']      = 'Неверных тегов не найдено.';
+$lang['admin clean']           = 'Безвозвратно удалить все неверныеые теги';
+$lang['toggle admin mode']     = 'Тег-редактор';
+$lang['no_admin']              = 'Ошибка. Только администраторы могу изменять теги.';
+$lang['search_section_title']  = 'Страницы с тегом';
+$lang['search_any_tag']        = 'Поиск тега: любой';
+$lang['search_all_tags']       = 'Поиск тега: все';
+$lang['search_filter_label']   = 'Фильтр по тегу';
+$lang['js']['notags']          = 'Тегов ещё нет.';
+$lang['js']['nopages']         = 'Страниц с этим тегом пока нет.';
+$lang['js']['admin_change_tag'] = 'Изменить тег';
+$lang['js']['admin_tagged_pages'] = 'Страницы с тегом';
+$lang['js']['admin_confirm']   = 'Подтвердить';
+$lang['js']['admin_cancel']    = 'Отменить';
+$lang['js']['admin_rename']    = 'Переименовать';
+$lang['js']['admin_delete']    = 'Удаление';
+$lang['js']['admin_warning_all'] = 'Предупреждение: изменения будут применены ко ВСЕМ тегам, независимо от фильтров таблицы!';
+$lang['js']['admin_sure']      = 'Вы уверены?';
+$lang['js']['admin_newtags']   = 'Пожалуйста, введите новое имя тега. Вы можете указать несколько тегов, разделённых запятыми, чтобы разбить тег на несколько.';
+$lang['js']['search_nofilter'] = 'ничего не найдено';
+$lang['tagjmp_error']          = 'Не найдено страниц с тегом %s';

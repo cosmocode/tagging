0acf68d24ce068faed1535268b8f27af5b8204d8
Hérisson grognon <dodoperso@laposte.net>
2021-02-24 14:20:20 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hérisson grognon <dodoperso@laposte.net>
+ */
+$lang['menu']                  = 'Afficher et modifier les étiquettes';
+$lang['admin tag']             = 'Nom de l\'étiquette';
+$lang['admin tags']            = 'Étiquettes';
+$lang['admin nonexistent page'] = 'Page inexistante étiquetée';
+$lang['admin occurrence']      = 'Occurrence';
+$lang['admin namespaces']      = 'Espaces de noms';
+$lang['admin taggers']         = 'Enregistré par';
+$lang['admin actions']         = 'Actions';
+$lang['admin save']            = 'Sauvegarder';
+$lang['admin find tag']        = 'Trouver une étiquette';
+$lang['admin new name']        = 'Nouveau nom';
+$lang['admin rename tag']      = 'Renommer l\'étiquette';
+$lang['admin rename']          = 'Renommer';
+$lang['admin filter']          = 'Filtrer par espace de nom';
+$lang['admin enter tag names'] = 'Veuillez saisir les actuel et nouveau noms d\'étiquette.';
+$lang['admin tag does not exists'] = 'Le nom d\'étiquette que vous avez saisi n\'existe pas.';
+$lang['admin renamed']         = 'Votre étiquette a été renommée.';
+$lang['admin delete_selected'] = 'Supprimer la sélection';
+$lang['admin delete']          = 'Supprimer';
+$lang['admin filter button']   = 'Filtrer';
+$lang['admin deleted']         = 'Vous avez retiré %d étiquettes de %d pages.';
+$lang['admin sort ascending']  = 'Trier par ordre croissant';
+$lang['admin sort descending'] = 'Trier par ordre décroissant';
+$lang['admin no invalid']      = 'Aucune étiquette invalide n\'a été trouvée.';
+$lang['admin clean']           = 'Supprimer définitivement tous les marquages invalides';
+$lang['toggle admin mode']     = 'Administrateur d\'étiquettes';
+$lang['no_admin']              = 'Erreur. Seuls les administrateurs peuvent modifier les étiquettes.';
+$lang['search_section_title']  = 'Pages étiquetées';
+$lang['search_suggestions']    = 'Étiquettes correspondantes';
+$lang['search_all_label']      = 'ET';
+$lang['search_any_label']      = 'OU';
+$lang['search_filter_label']   = 'Filtre d\'étiquettes';
+$lang['js']['notags']          = 'Pas encore d\'étiquettes.';
+$lang['js']['nopages']         = 'Pas encore de pages avec cette étiquette.';
+$lang['js']['admin_change_tag'] = 'Changer l\'étiquette';
+$lang['js']['admin_tagged_pages'] = 'Pages étiquetées';
+$lang['js']['admin_confirm']   = 'Confirmer';
+$lang['js']['admin_cancel']    = 'Annuler';
+$lang['js']['admin_rename']    = 'Renommer';
+$lang['js']['admin_delete']    = 'Supprimer';
+$lang['js']['admin_warning_all'] = 'Avertissement : Les changements seront appliqués à TOUS les tags, quels que soient les filtres des tableaux !';
+$lang['js']['admin_sure']      = 'Êtes-vous sûr(e) ?';
+$lang['js']['admin_newtags']   = 'Veuillez saisir le nouveau nom de l\'étiquette. Vous pouvez fournir plusieurs étiquettes séparées par des virgules pour diviser l\'étiquette en plusieurs.';
+$lang['js']['search_nofilter'] = 'Rien n\'a été trouvé.';
+$lang['tagjmp_error']          = 'Aucune page avec l\'étiquette %s n\'a été trouvée.';

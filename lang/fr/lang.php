82304e3e9783c7f85629d2b2e41dce760382c259
Mabrouk Belhout <mabroukb@gmail.com>
2022-05-12 12:00:09 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Mabrouk Belhout <mabroukb@gmail.com>
+ */
+$lang['menu']                  = 'Visualiser et modifier des Tags';
+$lang['admin tag']             = 'nom du Tag';
+$lang['admin tags']            = 'Tags';
+$lang['admin save']            = 'Sauvegarde';
+$lang['admin find tag']        = 'Rechercher un Tag';
+$lang['admin new name']        = 'Nouveau nom';
+$lang['admin rename tag']      = 'Renommer le Tag';
+$lang['admin rename']          = 'Renommer';
+$lang['admin delete_selected'] = 'Supprimer la sélection';
+$lang['admin delete']          = 'Supprimer';
+$lang['admin deleted']         = 'Vous avez supprimé %d tags sur %d pages';
+$lang['admin no invalid']      = 'Aucun tag non valide trouvé';
+$lang['admin clean']           = 'Supprimer définitivement les tags non valides';
